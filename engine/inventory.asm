d92374ca12e8f3c2544338fcf23ef5057df62339
SlithyMatt <slithymatt@hotmail.com>
2020-01-23 01:32:51 -0500

    started defining inventory data

--- engine/inventory.asm
+++ engine/inventory.asm
@@ -1,7 +1,29 @@
 .ifndef INVENTORY_INC
 INVENTORY_INC = 1
 
+INV_START_Y          = 0
+INV_TILEMAP          = 1
+INV_NUM_ITEMS        = 321
+INV_ITEM_WIDTH       = 322
+INV_ITEM_HEIGHT      = 323
+INV_ITEM_START_X     = 324
+INV_ITEM_STEP_X      = 325
+INV_ITEM_QUANT_X     = 326
+INV_ITEM_QUANT_WIDTH = 327
+INV_ITEM_CFG         = 328
+INV_MAX_ITEM_LABEL   = 16
+
+
+__inv_tilemap_addr:  .word 0
+__inv_scroll_up_x:   .byte 0
+__inv_scroll_up_y:   .byte 0
+__inv_scroll_down_x: .byte 0
+__inv_scroll_down_y: .byte 0
+
+
+
 init_inv:
+
 
    rts
 
